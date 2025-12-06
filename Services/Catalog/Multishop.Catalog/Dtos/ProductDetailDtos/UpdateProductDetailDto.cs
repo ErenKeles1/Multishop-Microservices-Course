7c715e625755b6a6036bd2c09c4f9fe4f0f5c2c2
ErenKeles1 <erenkeles741@gmail.com>
2025-12-07 01:21:40 +0300

    Add project files.

---
+++ Services/Catalog/Multishop.Catalog/Dtos/ProductDetailDtos/UpdateProductDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Multishop.Catalog.Dtos.ProductDetailDtos
+{
+    public class UpdateProductDetailDto
+    {
+        public string ProductDetailId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+        public string ProductID { get; set; }
+
+    }
+}
