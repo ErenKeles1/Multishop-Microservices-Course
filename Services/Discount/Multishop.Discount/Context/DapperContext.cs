057c7a7c42be30107d0c9cdbf55e40381f0db473
ErenKeles1 <erenkeles741@gmail.com>
2025-12-11 03:20:48 +0300

    Discount microservice with dapper and mssql

---
+++ Services/Discount/Multishop.Discount/Context/DapperContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Multishop.Discount.Entities;
+using System.Data;
+
+namespace Multishop.Discount.Context
+{
+    public class DapperContext:DbContext
+    {
+        private readonly IConfiguration _configuration;//Dapper için konfigürasyon
+        private readonly string _connectionString; 
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial Catalog=MultiShopDiscountDb;integrated" +
+                " Security=true");
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+    }
+}
